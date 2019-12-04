654638157cd4b3ec472a7ecff8e9f40b750f2502
grooves97 <45422778+grooves97@users.noreply.github.com>
2019-12-04 16:06:57 +0600

    image uploader

---
+++ src/app/shared/upload-image.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadImageService {
+
+  constructor(private http : HttpClient) { }
+
+  postFile(caption: string, fileToUpload: File){
+    const endpoint = "https://localhost:44384/api/UploadImage";
+    const formData: FormData = new FormData();
+    formData.append('Image', fileToUpload, fileToUpload.name);
+    formData.append('ImageCaption', caption);
+    return this.http.post(endpoint, formData);
+  }
+
+}
